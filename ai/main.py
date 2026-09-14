553cca3e73057be38bd584b95daea5c0dc956848
user <user@MacBook-Pro-user.local>
2026-09-14 16:29:45 +0300

    add retry ai request

--- ai/main.py
+++ ai/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, Depends, UploadFile, File
 from dotenv import load_dotenv
 
-from schemas import ClassifiedResponse
+from schemas import ClassifiedResponse, RetryClassifyRequest
 from service import AI_Serivce
 from semantic.vector_search import TextClassifier
 load_dotenv()
@@ -20,8 +20,8 @@ def ask_officiant(query: str, service: AI_Serivce = Depends(get_ai_service)):
 def get_dish_name_by_str(dish_name: str, service: AI_Serivce = Depends(get_ai_service)):
     return service.get_description(dish_name)
 
-@app.get("/retry/{dish_name}", response_model=ClassifiedResponse)
-def retry_get_dish_name_by_str(dish_name: str, prev_attempts: list[str], service: AI_Serivce = Depends(get_ai_service)):
+@app.post("/retry", response_model=ClassifiedResponse)
+def retry_get_dish_name_by_str(request: RetryClassifyRequest, service: AI_Serivce = Depends(get_ai_service)):
     return service.retry_get_description(dish_name, prev_attempts)
 
 @app.post("/classify_photo", response_model = ClassifiedResponse)
--- backend/app/routers/ai_router.py
+++ backend/app/routers/ai_router.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends, UploadFile, File
 
 from services.ai_service import AI_Service
-from schemas.ai import ImagesResponse, ClassifyResponse
+from schemas.ai import ImagesResponse, ClassifyResponse, RetryClassifyRequest
 from utils.instances import get_ai_service
 
 router = APIRouter(prefix="/ai", tags=["ai"])
@@ -13,6 +13,10 @@ def ask(query: str, service: AI_Service = Depends(get_ai_service)):
 @router.get("/get_name/{dish_name}", response_model = ImagesResponse)
 def get_name(dish_name: str, service: AI_Service = Depends(get_ai_service)):
     return service.get_dish_name_by_str(dish_name)
+
+@router.post("/retry_get_name", response_model=ImagesResponse)
+def retry_get_name(request: RetryClassifyRequest, service: AI_Serivce=Depends(get_ai_service)):
+    return service.retry_get_dish_name_by_str(request.dish_name, request.attempts)
 
 @router.post("/classify_photo", response_model = ClassifyResponse)
 def classify_photo(photo: UploadFile = File(...), service: AI_Service = Depends(get_ai_service)):
