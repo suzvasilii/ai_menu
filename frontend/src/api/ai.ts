57b31427235e66805dee61c168eba26a9fabf36c
user <user@MacBook-Pro-user.local>
2026-09-14 17:10:53 +0300

    integ ai engine

--- frontend/src/api/ai.ts
+++ frontend/src/api/ai.ts
@@ -1,6 +1,49 @@
 import api from './index'
 
+export interface RetryRequest{
+  dish_name: string
+  attempts: string[]
+}
+
+export interface DishResponse{
+  data_url:string
+}
+
+export interface DishesResponse{
+  images: DishResponse[]
+  dish_name:string,
+  categoty: string
+}
+
+export interface ClassifiedResponse{
+  dish_name:string,
+  category: string
+}
+
 export const aiApi = {
-  exchangeToken: (token: string) =>
-    api.post('/auth/verify', { token }),
+  askOfficiant: async (question: string): Promise<string> => {
+    const response = await api.get(`/ai/officiant/${question}`)
+    return response.data
+  },
+    
+  getDishPhoto: async(dish_name: string): Promise<DishesResponse> => {
+    const response = await api.get(`/ai/get_name/${dish_name}`)
+    return response.data
+  },
+
+  retryGetDishPhoto: async(request: RetryRequest): Promise<DishesResponse> => {
+    const response = await api.post('/ai/retry_get_name', request)
+    return response.data
+  },
+
+  classifyByPhoto: async (photo: File): Promise<ClassifiedResponse> => {
+      const formData = new FormData()
+      formData.append('photo', photo)
+      const response = await api.post('/ai/classify_photo', formData, {
+          headers: {
+              'Content-Type': 'multipart/form-data',
+          },
+      })
+      return response.data
+      }
 }
