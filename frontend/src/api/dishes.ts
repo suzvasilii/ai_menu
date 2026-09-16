ff66541d747b78a28fd22f7c54fd6ab97b25ab32
suzvasilii <vevjs@mail.ru>
2026-09-17 01:32:15 +0300

    debug

--- frontend/src/api/dishes.ts
+++ frontend/src/api/dishes.ts
@@ -3,12 +3,13 @@ import api from './index'
 export interface Dish {
     id: number
     name: string
+    category: string
     image_url: string
 }
 
 export const dishesApi = {
     getAll: async (): Promise<Dish[]> => {
-        const response = await api.get(`${API_URL}/get`)
+        const response = await api.get('/dish/get')
         return response.data.map((dish: Dish) => ({
             ...dish,
             image_url: `${BASE_URL}${dish.image_url}`
@@ -16,18 +17,18 @@ export const dishesApi = {
     },
 
     create: async (name: string): Promise<Dish> => {
-        const response = await api.post(`${API_URL}/create_by_name`, { name })
+        const response = await api.post('/dish/create_by_name', { name })
         return response.data
     },
 
     delete: async (id: number): Promise<void> =>{
-        await api.delete(`${API_URL}/del/${id}`)
+        await api.delete('/dish/del/${id}')
     },
 
     createByPhoto: async (file: File): Promise<void> => {
     const formData = new FormData()
     formData.append('file', file)
-    await axios.post(`${API_URL}/create_by_photo`, formData, {
+    await axios.post('/dish/create_by_photo', formData, {
         headers: {
             'Content-Type': 'multipart/form-data',
         },
